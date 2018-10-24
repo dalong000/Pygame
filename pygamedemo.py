95085a118d867d81ed1f57012088b95a98d1fac8
liangdalong <liangdalong2010@gmail.com>
2018-10-24 17:39:14 +0800

    bullet（子弹类）

--- pygamedemo.py
+++ pygamedemo.py
@@ -4,6 +4,20 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, bullet_surface, bullet_init_pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_surface
+        self.rect = self.image.get_rect()
+        self.rect.topleft = bullet_init_pos
+        self.speed = 8
+    #控制子弹移动
+    def update(self):
+        self.rect.top -= self.speed
+        if self.rect.top < -self.rect.height:
+            self.kill()
+
+
 class Hero(pygame.sprite.Sprite):
     def __init__(self, hero_surface, hero_init_pos):
         pygame.sprite.Sprite.__init__(self)
@@ -11,8 +25,29 @@ class Hero(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = hero_init_pos
         self.speed = 6
+        self.bullets1 = pygame.sprite.Group()
+
+    def single_shoot(self,bullet1_surface):
+        bullet1 = Bullet(bullet1_surface, self.rect.midtop)
+        self.bullets1.add(bullet1)
+
 
     def move(self,offset):
+        x = self.rect.left + offset[pygame.K_RIGHT] - offset[pygame.K_LEFT]
+        y = self.rect.top + offset[pygame.K_DOWN] - offset[pygame.K_UP]
+        if x < 0:
+            self.rect.left = 0
+        elif x > SCREEN_WIDTH - self.rect.width:
+            self.rect.left = SCREEN_WIDTH - self.rect.width
+        else:
+            self.rect.left = x
+
+        if y < 0:
+            self.rect.top = 0
+        elif y > SCREEN_HEIGHT - self.rect.height:
+            self.rect.top = SCREEN_HEIGHT - self.rect.height
+        else:
+            self.rect.top = y
 
 
 
@@ -21,9 +56,18 @@ class Hero(pygame.sprite.Sprite):
 SCREEN_WIDTH = 390
 SCREEN_HEIGHT = 520
 
+#定义画面帧率
+FRAME_RATE = 60
+
+#定义动画周期（帧数）
+ANIMATE_CYCLE = 30
+
 #周期变量
 ticks = 0
 #dict
+
+
+clock = pygame.time.Clock()
 offset = {pygame.K_LEFT:0, pygame.K_RIGHT:0, pygame.K_UP:0, pygame.K_DOWN:0}
 
 #初始化游戏
@@ -33,27 +77,54 @@ pygame.display.set_caption('gamedemo')#设置标题
 #载入背景
 background = pygame.image.load('resources/image/background.PNG')
 
-#载入资源图片
+#载入飞机图片
 shoot_img = pygame.image.load('resources/image/shoot.png')
 #使用subsurface剪切载入的图片
+hero_surface = []
+hero_surface.append(shoot_img.subsurface(pygame.Rect(10,100,99,126)))
+hero_surface.append(shoot_img.subsurface(pygame.Rect(40,100,99,126)))
+'''
 hero1_rect = pygame.Rect(10,100,99,126)
 hero2_rect = pygame.Rect(40,100,99,126)
 hero1 = shoot_img.subsurface(hero1_rect)
 hero2 = shoot_img.subsurface(hero2_rect)
+'''
 hero_pos = [200 ,500]
 
-clock = pygame.time.Clock()
 
+#创建玩家
+hero = Hero(hero_surface[0],hero_pos)
+
+
+#bullet1
+bullet1_surface = shoot_img.subsurface(pygame.Rect(12,234,9,21))
+
+
+
+#事件循环
 while True:
-    clock.tick(60)
+    #clock.tick(60)
+    #控制游戏最大帧率
+    clock.tick(FRAME_RATE)
     #绘制背景
     screen.blit(background, (0,0))
 
-    if ticks % 50 < 25:
-        screen.blit(hero1,hero_pos)
-    else:
-        screen.blit(hero2,hero_pos)
+    if ticks >=ANIMATE_CYCLE:
+        ticks = 0
+    hero.image = hero_surface[ticks//(ANIMATE_CYCLE//2)]
+
+    #绘制飞机
+    screen.blit(hero.image,hero.rect)
     ticks += 1
+
+
+    #射击
+    if ticks % 10 == 0:
+        hero.single_shoot(bullet1_surface)
+    #控制子弹
+    hero.bullets1.update()
+    #绘制子弹
+    hero.bullets1.draw(screen)
 
     #更新屏幕
     pygame.display.update()
@@ -73,19 +144,5 @@ while True:
                 offset[event.key] = 0
 
 
-    hero_x = hero_pos[0] + offset[pygame.K_RIGHT] - offset[pygame.K_LEFT]
-    hero_y = hero_pos[1] + offset[pygame.K_DOWN] - offset[pygame.K_UP]
-    if hero_pos[0] < 0:
-        hero_pos[0] = 0
-    elif hero_x > SCREEN_WIDTH - hero1_rect.width:
-        hero_pos[0] = SCREEN_WIDTH - hero1_rect.width
-    else:
-        hero_pos[0] = hero_x
-
-    if hero_y < 0:
-        hero_pos[1] = 0
-    elif hero_y > SCREEN_HEIGHT - hero1_rect.height:
-        hero_pos[1] = SCREEN_HEIGHT - hero1_rect.height
-    else:
-        hero_pos[1] = hero_y
+    hero.move(offset)
 
